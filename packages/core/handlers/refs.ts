4ab9009ba5e9db79eb5e07707b02b966d7e78520
木荣 <372638156@qq.com>
2022-06-07 11:09:04 +0800

    fix: multiple return `$ref`

--- packages/core/handlers/refs.ts
+++ packages/core/handlers/refs.ts
@@ -2,6 +2,7 @@ import j from 'jscodeshift'
 import type { Collector } from '../types'
 
 export function refsHandler(astCollection: j.Collection, collector: Collector): j.Collection {
+  const refs: string[] = []
   astCollection
     .find(j.MemberExpression)
     .filter((path) => {
@@ -14,20 +15,24 @@ export function refsHandler(astCollection: j.Collection, collector: Collector): 
         const parentObject: j.Identifier | j.MemberExpression = j.memberExpression(property, j.identifier('value'))
         path.parent.parent.replace(parentObject)
 
-        collector.setupFn.body.body.push(
-          j.variableDeclaration('const', [
-            j.variableDeclarator(
-              j.identifier(property.name),
-              j.callExpression(
-                j.identifier('ref'),
-                [j.identifier('null')],
+        // fix: multiple return `ref`
+        if (!refs.includes(property.name)) {
+          collector.setupFn.body.body.push(
+            j.variableDeclaration('const', [
+              j.variableDeclarator(
+                j.identifier(property.name),
+                j.callExpression(
+                  j.identifier('ref'),
+                  [j.identifier('null')],
+                ),
               ),
-            ),
-          ]),
-        );
-        (collector.returnStatement.argument as j.ObjectExpression).properties.push(
-          j.property('init', j.identifier(property.name), j.identifier(property.name)),
-        )
+            ]),
+          );
+          (collector.returnStatement.argument as j.ObjectExpression).properties.push(
+            j.property('init', j.identifier(property.name), j.identifier(property.name)),
+          )
+          refs.push(property.name)
+        }
       }
     })
   return astCollection
